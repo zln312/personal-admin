13909f99d8ddf85715d279230c8859bbd0f16f25
zln <714323486@qq.com>
2021-02-07 19:35:36 +0800

    a

--- src/layout/components/nav_menu/index.vue
+++ src/layout/components/nav_menu/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-06 22:17:06
- * @LastEditTime: 2021-02-06 23:36:00
+ * @LastEditTime: 2021-02-07 19:34:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \personal-admin\src\layout\components\nav_menu\index.vue
@@ -20,11 +20,6 @@
           <i class="el-icon-location"></i>
           <span>导航一</span>
         </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
         <el-menu-item-group title="分组2">
           <el-menu-item index="1-3">选项3</el-menu-item>
         </el-menu-item-group>
